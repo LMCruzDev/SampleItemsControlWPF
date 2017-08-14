51be8c872020858eec98814e821b10ebd03552d6
DESKTOP-EJB1UJL\Lucas Cruz <moreira552@msn.com>
2017-08-14 01:19:47 -0300

    Ajuste de namespace

--- SampleItemsControl/Properties/AssemblyInfo.cs
+++ SampleItemsControl/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Teste")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SampleItemsControl")]
+[assembly: AssemblyDescription("Sample of items control with Datagrid using WPF")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Teste")]
+[assembly: AssemblyProduct("SampleItemsControl")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
